c336240b78f236f8c46e34266f00a5cf246be848
Steve Schwab <schwabs@umich.edu>
2024-06-04 11:59:52 -0700

    check car id middleware function

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,5 +1,21 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const Cars = require('./cars-model')
+
+const checkCarId = async (req, res, next) => {
+  try {
+    const id = req.params.id
+    const car = await Cars.getById(id)
+
+    if (!car) {
+      return next({ 
+        status: 404, 
+        message: `car with id ${id} is not found` 
+      })
+    }
+    
+    next()
+  } catch (error) {
+    next(error)
+  }
 }
 
 const checkCarPayload = (req, res, next) => {
