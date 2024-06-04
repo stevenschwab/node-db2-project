cf0b47021df874c0d0e8dc9d3b0c912938c5b649
Steve Schwab <schwabs@umich.edu>
2024-06-03 17:22:52 -0700

    write cars schema

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,28 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments();
+    tbl.string('vin', 255)
+      .unique()
+      .notNullable();
+    tbl.string('make', 255)
+      .notNullable();
+    tbl.string('model', 255)
+      .notNullable();
+    tbl.decimal('mileage')
+      .notNullable();
+    tbl.string('title', 255);
+    tbl.string('transmission', 255);
+  })
 };
 
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars');
 };
