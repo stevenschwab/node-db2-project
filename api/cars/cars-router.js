e0105968d6ea8b829a901e727b17c0d7d955a533
Steve Schwab <schwabs@umich.edu>
2024-06-03 17:52:46 -0700

    export the middleware functions

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,5 +1,18 @@
 const express = require('express')
+const Cars = require('./cars-model')
 
 const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    
+})
+
+router.get('/:id', (req, res, next) => {
+
+})
+
+router.post('/', (req, res, next) => {
+
+})
 
 module.exports = router
