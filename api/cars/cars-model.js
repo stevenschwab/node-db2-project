72eb3ae2932648d1cd59fa6877291fea718a1773
Steve Schwab <schwabs@umich.edu>
2024-06-04 12:42:17 -0700

    function to get vin in db

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -8,6 +8,10 @@ const getById = id => {
   return db('cars').where('id', id).first()
 }
 
+const getByVin = vin => {
+  return db('cars').where('vin', vin).first()
+}
+
 const create = async car => {
   const [id] = await db('cars').insert(car)
   return getById(id)
